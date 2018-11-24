9d6e37ee9ad7887f9dcf99eee66b329481631e73
Mark Karpeles <magicaltux@gmail.com>
2018-11-24 11:22:43 +0900

    (zval doc wip)

---
+++ doc/zval.go
@@ -0,0 +1,8 @@
+# ZVal
+
+ZVals are generic pointer containers for all sorts of values.
+
+## Val
+
+ZVals contains values of type Val. Val can be any of ZNull, ZBool, ZInt, ZFloat, ZString, ZArray, ZObject or ZResource. They can also contain other ZVals, as reference.
+
