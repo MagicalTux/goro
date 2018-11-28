ca338d81e7cf9efac9f972a84dd3db2484e7a630
Mark Karpeles <magicaltux@gmail.com>
2018-11-28 14:23:25 +0900

    Issue #8 : drop support for keccak256 and fix macos uname / add fallback uname

--- ext/standard/uname.go
+++ ext/standard/uname.go
@@ -1,4 +1,4 @@
-// +build darwin
+// +build !linux,!darwin
 
 package standard
 
@@ -9,7 +9,8 @@ import (
 	"github.com/MagicalTux/goro/core"
 )
 
-// this is an approximate estimation of php_uname()
+// fallback uname
+
 func fncUname(ctx core.Context, args []*core.ZVal) (*core.ZVal, error) {
 	var arg string
 	_, err := core.Expand(ctx, args, &arg)
@@ -19,7 +20,7 @@ func fncUname(ctx core.Context, args []*core.ZVal) (*core.ZVal, error) {
 
 	switch arg {
 	case "s":
-		return core.ZString("Darwin").ZVal(), nil
+		return core.ZString(runtime.GOOS).ZVal(), nil
 	case "n":
 		n, err := os.Hostname()
 		if err != nil {
@@ -38,6 +39,6 @@ func fncUname(ctx core.Context, args []*core.ZVal) (*core.ZVal, error) {
 			return nil, err
 		}
 		// return full uname, ie "s n r v m"
-		return core.ZString("Darwin " + n + " " + runtime.GOARCH).ZVal(), nil
+		return core.ZString(runtime.GOOS + " " + n + " " + runtime.GOARCH).ZVal(), nil
 	}
 }
