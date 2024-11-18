b0b5eb96faf71a884cbdf1d4840883da357c463a
Ronald Casili <nvlled@gmail.com>
2024-11-18 20:00:58 +0800

    Add phpt test runner

---
+++ sapi/php-test/main.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"runtime/debug"
+	"strings"
+
+	"github.com/MagicalTux/goro/core/compiler"
+	"github.com/MagicalTux/goro/core/phpctx"
+	"github.com/MagicalTux/goro/core/phpv"
+	"github.com/MagicalTux/goro/core/tokenizer"
+	_ "github.com/MagicalTux/goro/ext/ctype"
+	_ "github.com/MagicalTux/goro/ext/date"
+	_ "github.com/MagicalTux/goro/ext/gmp"
+	_ "github.com/MagicalTux/goro/ext/hash"
+	_ "github.com/MagicalTux/goro/ext/json"
+	_ "github.com/MagicalTux/goro/ext/pcre"
+	_ "github.com/MagicalTux/goro/ext/standard"
+	"github.com/andreyvit/diff"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		println("need .phpt filenames to run")
+		os.Exit(1)
+	}
+	for _, fpath := range os.Args[1:] {
+		stat, err := os.Stat(fpath)
+		if err != nil {
+			panic(err)
+		}
+		if stat.IsDir() {
+			files, err := os.ReadDir(fpath)
+			if err != nil {
+				panic(err)
+			}
+			for _, f := range files {
+				if _, err := runTest(filepath.Join(fpath, f.Name())); err != nil {
+					log.Printf("failed to run file: %s", err)
+					os.Exit(1)
+				}
+			}
+		} else {
+			if _, err := runTest(fpath); err != nil {
+				log.Printf("failed to run file: %s", err)
+				os.Exit(1)
+			}
+		}
+	}
+}
+
+type phptest struct {
+	f      *os.File
+	reader *bufio.Reader
+	output *bytes.Buffer
+	name   string
+	path   string
+	req    *http.Request
+
+	p *phpctx.Process
+}
+
+type skipError struct{}
+
+func (s skipError) Error() string {
+	return "test skipped"
+}
+
+var skipTest skipError
+
+func (p *phptest) handlePart(part string, b *bytes.Buffer) error {
+	switch part {
+	case "TEST":
+		testName := strings.TrimSpace(b.String())
+		p.name += ": " + testName
+		return nil
+	case "CREDITS":
+		// is there something we should do with this?
+		return nil
+	case "GET":
+		p.req.URL.RawQuery = strings.TrimRight(b.String(), "\r\n")
+		return nil
+	case "POST":
+		// we need a new request with the post data
+		p.req = httptest.NewRequest("POST", "/"+path.Base(p.path), bytes.NewBuffer(bytes.TrimRight(b.Bytes(), "\r\n")))
+		p.req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		return nil
+	case "FILE":
+		// pass data to the engine
+		g := phpctx.NewGlobalReq(p.req, p.p)
+		g.SetOutput(p.output)
+		g.Chdir(phpv.ZString(path.Dir(p.path))) // chdir execution to path
+
+		t := tokenizer.NewLexer(b, p.path)
+		c, err := compiler.Compile(g, t)
+		if err != nil {
+			return err
+		}
+		_, err = c.Run(g)
+		g.Close()
+		return phpv.FilterError(err)
+	case "EXPECT":
+		// compare p.output with b
+		out := bytes.TrimSpace(p.output.Bytes())
+		exp := bytes.TrimSpace(b.Bytes())
+
+		if bytes.Compare(out, exp) != 0 {
+			return fmt.Errorf("output not as expected!\n%s", diff.LineDiff(string(exp), string(out)))
+		}
+		return nil
+	case "SKIPIF":
+		t := tokenizer.NewLexer(b, p.path)
+		g := phpctx.NewGlobal(context.Background(), p.p)
+		output := &bytes.Buffer{}
+		g.SetOutput(output)
+		c, err := compiler.Compile(g, t)
+		if err != nil {
+			return err
+		}
+		_, err = c.Run(g)
+		err = phpv.FilterError(err)
+		if err != nil {
+			return err
+		}
+		if bytes.HasPrefix(output.Bytes(), []byte("skip ")) {
+			return skipTest
+		}
+		return nil
+	case "INI", "EXPECTF", "EXTENSIONS":
+		// TODO
+		return skipTest
+	case "XFAIL":
+		// TODO but safe to ignore
+		return nil
+	default:
+		return fmt.Errorf("unhandled part type %s for test", part)
+	}
+}
+
+func runTest(fpath string) (p *phptest, err error) {
+	println("running file:", fpath)
+	p = &phptest{output: &bytes.Buffer{}, name: fpath, path: fpath}
+
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("failed to run: %s\n%s", r, debug.Stack())
+		}
+	}()
+
+	// read & parse test file
+	p.f, err = os.Open(fpath)
+	if err != nil {
+		return
+	}
+	defer p.f.Close()
+	p.reader = bufio.NewReader(p.f)
+
+	var b *bytes.Buffer
+	var part string
+
+	// prepare env
+	p.p = phpctx.NewProcess("test")
+	p.req = httptest.NewRequest("GET", "/"+path.Base(fpath), nil)
+	r := regexp.MustCompile("^--([A-Z]+)--$")
+
+	for {
+		lin, err := p.reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return p, err
+		}
+		if strings.HasPrefix(lin, "--") {
+			lin_trimmed := strings.TrimRight(lin, "\r\n")
+
+			if sub := r.FindSubmatch([]byte(lin_trimmed)); sub != nil {
+				thing := string(sub[1])
+				// start of a new thing?
+				if b != nil {
+					err := p.handlePart(part, b)
+					if err != nil {
+						return p, err
+					}
+				}
+				b = &bytes.Buffer{}
+				part = thing
+				continue
+			}
+		}
+
+		if b == nil {
+			return p, fmt.Errorf("malformed test file %s", fpath)
+		}
+		b.Write([]byte(lin))
+	}
+	if b != nil {
+		err := p.handlePart(part, b)
+		if err != nil {
+			return p, err
+		}
+	}
+
+	return p, nil
+}
