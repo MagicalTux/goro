c2ca48eb7d02cd2e49c7a3fabbdcf2db19f1b0d3
Ronald Casili <nvlled@gmail.com>
2024-12-06 20:16:56 +0800

    Add array sorting functions

--- ext/standard/array_sort.go
+++ ext/standard/array_sort.go
@@ -17,42 +17,82 @@ func fncArraySort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
 		return nil, ctx.FuncError(err)
 	}
 
-	caseInsensitive := false
-	sortFlags := SORT_REGULAR
-
-	if sortFlagsArg != nil {
-		sortFlags = *sortFlagsArg
-		caseInsensitive = sortFlags&SORT_FLAG_CASE != 0
-		sortFlags &= ^SORT_FLAG_CASE
-	}
-
-	var values []*phpv.ZVal
+	var entries []compareEntry
 	for _, v := range (array).Iterate(ctx) {
-		values = append(values, v)
+		entries = append(entries, compareEntry{
+			item: v,
+		})
 	}
 
-	sortBy := zValuesSorter{ctx, values, caseInsensitive}
-	sortFn := sortBy.regular
-
-	switch sortFlags {
-	case SORT_STRING, SORT_LOCALE_STRING:
-		sortFn = sortBy.stringly
-	case SORT_NATURAL:
-		sortFn = sortBy.naturally
-	case SORT_NUMERIC:
-		sortFn = sortBy.numerically
-	case SORT_REGULAR:
-		sortFn = sortBy.regular
-	}
-
-	sort.Slice(values, sortFn)
+	arraySort(ctx, entries, sortFlagsArg)
 
 	if err = array.Clear(ctx); err != nil {
 		return nil, err
 	}
 
-	for _, v := range values {
-		array.OffsetSet(ctx, nil, v)
+	for _, v := range entries {
+		array.OffsetSet(ctx, nil, v.item)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+// > func bool rsort ( array &$array [, int $sort_flags = SORT_REGULAR ] )
+func fncArrayRSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
+	var array *phpv.ZArray
+	var sortFlagsArg *phpv.ZInt
+	_, err := core.Expand(ctx, args, &array, &sortFlagsArg)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	var entries []compareEntry
+	for _, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: v,
+		})
+	}
+
+	arraySort(ctx, entries, sortFlagsArg)
+
+	if err = array.Clear(ctx); err != nil {
+		return nil, err
+	}
+
+	for _, v := range iterateBackwards(entries) {
+		array.OffsetSet(ctx, nil, v.item)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+// > func bool usort ( array &$array [, int $sort_flags = SORT_REGULAR ] )
+func fncArrayUSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
+	var array *phpv.ZArray
+	var compareFunc phpv.Callable
+	_, err := core.Expand(ctx, args, &array, &compareFunc)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	var entries []compareEntry
+	for _, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: v,
+		})
+	}
+
+	err = arrayUSort(ctx, entries, compareFunc)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	if err = array.Clear(ctx); err != nil {
+		return nil, err
+	}
+
+	for _, v := range entries {
+		array.OffsetSet(ctx, nil, v.item)
 	}
 
 	return phpv.ZTrue.ZVal(), nil
@@ -67,6 +107,174 @@ func fncArrayKSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
 		return nil, ctx.FuncError(err)
 	}
 
+	var entries []compareEntry
+	for k, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: k,
+			data: v,
+		})
+	}
+
+	arraySort(ctx, entries, sortFlagsArg)
+
+	for _, entry := range entries {
+		k := entry.item
+		v := entry.data
+		array.OffsetUnset(ctx, k)
+		array.OffsetSet(ctx, k, v)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+// > func bool krsort ( array &$array [, int $sort_flags = SORT_REGULAR ] )
+func fncArrayKRSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
+	var array *phpv.ZArray
+	var sortFlagsArg *phpv.ZInt
+	_, err := core.Expand(ctx, args, &array, &sortFlagsArg)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	var entries []compareEntry
+	for k, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: k,
+			data: v,
+		})
+	}
+
+	arraySort(ctx, entries, sortFlagsArg)
+
+	for _, entry := range iterateBackwards(entries) {
+		k := entry.item
+		v := entry.data
+		array.OffsetUnset(ctx, k)
+		array.OffsetSet(ctx, k, v)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+// > func bool asort ( array &$array [, int $sort_flags = SORT_REGULAR ] )
+func fncArrayASort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
+	var array *phpv.ZArray
+	var sortFlagsArg *phpv.ZInt
+	_, err := core.Expand(ctx, args, &array, &sortFlagsArg)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	var entries []compareEntry
+	for k, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: v,
+			data: k,
+		})
+	}
+
+	arraySort(ctx, entries, sortFlagsArg)
+
+	for _, entry := range entries {
+		k := entry.data
+		v := entry.item
+		array.OffsetUnset(ctx, k)
+		array.OffsetSet(ctx, k, v)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+// > func bool arsort ( array &$array [, int $sort_flags = SORT_REGULAR ] )
+func fncArrayARSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
+	var array *phpv.ZArray
+	var sortFlagsArg *phpv.ZInt
+	_, err := core.Expand(ctx, args, &array, &sortFlagsArg)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	var entries []compareEntry
+	for k, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: v,
+			data: k,
+		})
+	}
+
+	arraySort(ctx, entries, sortFlagsArg)
+
+	for _, entry := range iterateBackwards(entries) {
+		k := entry.data
+		v := entry.item
+		array.OffsetUnset(ctx, k)
+		array.OffsetSet(ctx, k, v)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+// > func bool natsort ( array &$array [, int $sort_flags = SORT_REGULAR ] )
+func fncArrayNatSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
+	var array *phpv.ZArray
+	var sortFlagsArg *phpv.ZInt
+	_, err := core.Expand(ctx, args, &array, &sortFlagsArg)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	var entries []compareEntry
+	for k, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: v,
+			data: k,
+		})
+	}
+
+	sortFlags := SORT_NATURAL
+	arraySort(ctx, entries, &sortFlags)
+
+	for _, entry := range entries {
+		k := entry.data
+		v := entry.item
+		array.OffsetUnset(ctx, k)
+		array.OffsetSet(ctx, k, v)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+// > func bool natcasesort ( array &$array [, int $sort_flags = SORT_REGULAR ] )
+func fncArrayNatCaseSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
+	var array *phpv.ZArray
+	var sortFlagsArg *phpv.ZInt
+	_, err := core.Expand(ctx, args, &array, &sortFlagsArg)
+	if err != nil {
+		return nil, ctx.FuncError(err)
+	}
+
+	var entries []compareEntry
+	for k, v := range (array).Iterate(ctx) {
+		entries = append(entries, compareEntry{
+			item: v,
+			data: k,
+		})
+	}
+
+	sortFlags := SORT_NATURAL | SORT_FLAG_CASE
+	arraySort(ctx, entries, &sortFlags)
+
+	for _, entry := range entries {
+		k := entry.data
+		v := entry.item
+		array.OffsetUnset(ctx, k)
+		array.OffsetSet(ctx, k, v)
+	}
+
+	return phpv.ZTrue.ZVal(), nil
+}
+
+func arraySort(ctx phpv.Context, entries []compareEntry, sortFlagsArg *phpv.ZInt) {
 	caseInsensitive := false
 	sortFlags := SORT_REGULAR
 
@@ -76,12 +284,7 @@ func fncArrayKSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
 		sortFlags &= ^SORT_FLAG_CASE
 	}
 
-	var keys []*phpv.ZVal
-	for k := range (array).Iterate(ctx) {
-		keys = append(keys, k)
-	}
-
-	sortBy := zValuesSorter{ctx, keys, caseInsensitive}
+	sortBy := zSortComparer{ctx, entries, caseInsensitive}
 	sortFn := sortBy.regular
 
 	switch sortFlags {
@@ -95,37 +298,51 @@ func fncArrayKSort(ctx phpv.Context, args []*phpv.ZVal) (*phpv.ZVal, error) {
 		sortFn = sortBy.regular
 	}
 
-	sort.Slice(keys, sortFn)
-
-	for _, k := range keys {
-		v, _ := array.OffsetGet(ctx, k)
-		array.OffsetUnset(ctx, k)
-		array.OffsetSet(ctx, k, v)
-	}
-
-	return phpv.ZTrue.ZVal(), nil
+	sort.Slice(entries, sortFn)
 }
 
-type zValuesSorter struct {
+func arrayUSort(ctx phpv.Context, entries []compareEntry, compare phpv.Callable) error {
+	var err error
+	// TODO: fix, items are being duplicated
+	sort.Slice(entries, func(i, j int) bool {
+		if err != nil {
+			return false
+		}
+		a := entries[i].item
+		b := entries[j].item
+
+		var ret *phpv.ZVal
+		ret, err = compare.Call(ctx, []*phpv.ZVal{a, b})
+		return ret.AsInt(ctx) < 0
+	})
+
+	return err
+}
+
+type compareEntry struct {
+	item *phpv.ZVal
+	data *phpv.ZVal
+}
+type zSortComparer struct {
 	ctx             phpv.Context
-	values          []*phpv.ZVal
+	values          []compareEntry
 	caseInsensitive bool
 }
 
-func (zv zValuesSorter) regular(i, j int) bool {
-	cmp, _ := core.Compare(zv.ctx, zv.values[i], zv.values[j])
+func (zv zSortComparer) regular(i, j int) bool {
+	cmp, _ := core.Compare(zv.ctx, zv.values[i].item, zv.values[j].item)
 	return cmp < 0
 }
 
-func (zv zValuesSorter) numerically(i, j int) bool {
-	a := zv.values[i].AsInt(zv.ctx)
-	b := zv.values[j].AsInt(zv.ctx)
+func (zv zSortComparer) numerically(i, j int) bool {
+	a := zv.values[i].item.AsInt(zv.ctx)
+	b := zv.values[j].item.AsInt(zv.ctx)
 	return a < b
 }
 
-func (zv zValuesSorter) stringly(i, j int) bool {
-	a := string(zv.values[i].AsString(zv.ctx))
-	b := string(zv.values[j].AsString(zv.ctx))
+func (zv zSortComparer) stringly(i, j int) bool {
+	a := string(zv.values[i].item.AsString(zv.ctx))
+	b := string(zv.values[j].item.AsString(zv.ctx))
 	if zv.caseInsensitive {
 		a = strings.ToLower(a)
 		b = strings.ToLower(b)
@@ -133,8 +350,8 @@ func (zv zValuesSorter) stringly(i, j int) bool {
 	return strings.Compare(a, b) < 0
 }
 
-func (zv zValuesSorter) naturally(i, j int) bool {
-	a := []byte(zv.values[i].AsString(zv.ctx))
-	b := []byte(zv.values[j].AsString(zv.ctx))
+func (zv zSortComparer) naturally(i, j int) bool {
+	a := []byte(zv.values[i].item.AsString(zv.ctx))
+	b := []byte(zv.values[j].item.AsString(zv.ctx))
 	return natCmp(a, b, !zv.caseInsensitive) < 0
 }
--- ext/standard/common.go
+++ ext/standard/common.go
@@ -1,5 +1,7 @@
 package standard
 
+import "iter"
+
 func deref[T any](ptr *T, defValue T) T {
 	if ptr == nil {
 		return defValue
@@ -12,4 +14,14 @@ func ifElse[T any](cond bool, consequence, alternative T) T {
 		return consequence
 	}
 	return alternative
+}
+
+func iterateBackwards[T any](xs []T) iter.Seq2[int, T] {
+	return func(yield func(int, T) bool) {
+		for i := len(xs) - 1; i >= 0; i-- {
+			if !yield(i, xs[i]) {
+				break
+			}
+		}
+	}
 }
